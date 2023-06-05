2a6f71a228d79a1d4f30fd5abba2457d002d1b6d
Omprakash Rahangdale <35596374+OmprakashR@users.noreply.github.com>
2023-06-05 13:06:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+gsap.to(".box-1", {right: 0, x: 0, duration: 1});
+gsap.to(".box-2", {top: 0, y: 0, duration: 1});
+gsap.to(".box-3", {bottom: 0, y: 0, duration: 1});
+gsap.to(".box-4", {left: 0, x: 0, duration: 1});
